3f182e092866c6f1078e7d1f4693cf6722b8c680
吕佐康 <lvzk@techsure.com.cn>
2021-05-27 17:01:12 +0800

    --task=1004540 --user=lvzk proxy-执行作业接口 https://www.tapd.cn/54247054/s/1151764

--- src/main/java/com/techsure/autoexecproxy/core/ExecManager.java
+++ src/main/java/com/techsure/autoexecproxy/core/ExecManager.java
@@ -28,7 +28,7 @@ public class ExecManager {
         String filePath = Config.PARAM_PATH() + File.separator + getJobPath(commandVo.getJobId(),new StringBuilder()) + File.separator + "params.json";
         FileUtil.saveFile(commandVo.getConfig(),filePath,"","");
         //set command
-        commandVo.setCommandList(Collections.singletonList(String.format("'python3 /app/autoexec/bin/autoexec --jobid \"%s\" --execuser \"%s\" -p %s'",commandVo.getJobId(),commandVo.getExecUser(),filePath)));
+        commandVo.setCommandList(Collections.singletonList(String.format("/bin/sh -c 'python3 /app/autoexec/bin/autoexec --jobid \"%s\" --execuser \"%s\" -p %s'",commandVo.getJobId(),commandVo.getExecUser(),filePath)));
         ExecProcessCommand processCommand = new ExecProcessCommand(commandVo);
         CommonThreadPool.execute(processCommand);
     }

99ac40a682ded1fcfd3f14b660f37cce9c42df91
吕佐康 <lvzk@techsure.com.cn>
2021-07-20 16:25:42 +0800

    重置作业

---
+++ src/main/java/com/techsure/autoexecproxy/api/JobAllResetApi.java
@@ -0,0 +1,59 @@
+package com.techsure.autoexecproxy.api;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.techsure.autoexecproxy.common.config.Config;
+import com.techsure.autoexecproxy.constvalue.ApiParamType;
+import com.techsure.autoexecproxy.core.ExecManager;
+import com.techsure.autoexecproxy.restful.annotation.Input;
+import com.techsure.autoexecproxy.restful.annotation.Output;
+import com.techsure.autoexecproxy.restful.annotation.Param;
+import com.techsure.autoexecproxy.restful.core.privateapi.PrivateApiComponentBase;
+import com.techsure.autoexecproxy.util.FileUtil;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.bson.Document;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.io.File;
+import java.util.Objects;
+
+
+/**
+ * @author lvzk
+ * @since 2021/7/20 15:31
+ **/
+@Component
+public class JobAllResetApi extends PrivateApiComponentBase {
+    @Override
+    public String getName() {
+        return "重置作业";
+    }
+
+    @Resource
+    MongoTemplate mongoTemplate;
+
+    @Input({
+            @Param(name = "jobId", type = ApiParamType.STRING, desc = "作业Id", isRequired = true)
+    })
+    @Output({
+    })
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        Long jobId = jsonObj.getLong("jobId");
+        Document document = new Document();
+        document.put("jobId", jobId.toString());
+        Document result = mongoTemplate.getCollection("node_status").findOneAndDelete(document);
+        //删除对应status文件记录
+        String nodeStatusPath = Config.LOG_PATH() + File.separator + ExecManager.getJobPath(jobId.toString(), new StringBuilder()) + File.separator + "status";
+        FileUtil.deleteDirectoryOrFile(nodeStatusPath);
+        return null;
+    }
+
+    @Override
+    public String getToken() {
+        return "/job/all/reset";
+    }
+}

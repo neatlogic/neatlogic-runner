a135070254abe74a597a944c2be4d71248657eb7
吕佐康 <lvzk@techsure.com.cn>
2021-07-30 18:05:27 +0800

    update

--- src/main/java/com/techsure/autoexecproxy/api/JobPhaseNodeStatusResetApi.java
+++ src/main/java/com/techsure/autoexecproxy/api/JobPhaseNodeStatusResetApi.java
@@ -65,7 +65,7 @@ public class JobPhaseNodeStatusResetApi extends PrivateApiComponentBase {
                 //删除对应status文件记录
                 String nodeStatusPath = Config.LOG_PATH() + File.separator + ExecManager.getJobPath(jobId.toString(), new StringBuilder()) + File.separator + "status" + File.separator + phase + File.separator;
                 if (Objects.equals(execMode, "target")) {
-                    nodeStatusPath += host + "-" + port + node.getString("resourceId") + ".json";
+                    nodeStatusPath += host + "-" + port + "-" +node.getString("resourceId") + ".json";
                 } else {
                     nodeStatusPath += "local-0.json";
                 }

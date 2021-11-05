7a22dd6b0f88c9df19ebdc400c2f792a248fc73d
longrf <longrf@techsure.com.cn>
2021-11-05 16:23:07 +0800

    --story=1005084 --user=龙儒锋 Tagent管理功能迁移 https://www.tapd.cn/54247054/s/1182920

--- src/main/java/com/techsure/autoexecrunner/api/tagent/TagentUpgradeApi.java
+++ src/main/java/com/techsure/autoexecrunner/api/tagent/TagentUpgradeApi.java
@@ -30,66 +30,70 @@ public class TagentUpgradeApi extends PublicBinaryStreamApiComponentBase {
     @Override
     public Object myDoService(JSONObject paramObj, HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-        TagentResultHandler handler = new TagentResultHandler();
+        try {
+            TagentResultHandler handler = new TagentResultHandler();
 
-        //获得升级文件
-        String fileName = paramObj.getString("fileName");
-        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
-        MultipartFile multipartFile = multipartRequest.getFile(fileName);
-        if (multipartFile == null) {
-            throw new TagentPkgNotFoundException(fileName);
-        }
-        InputStream input = multipartFile.getInputStream();
-
-        //获得os类型
-        TagentClient tagentClient = new TagentClient(paramObj.getString("ip"), Integer.parseInt(paramObj.getString("port")), RC4Util.decrypt(paramObj.getString("credential").substring(4)));
-        String osType = tagentClient.getAgentOsType();
-
-        int uploadStatus = tagentClient.upload(input, fileName, "$TAGENT_BASE/tmp", null);
-
-        //兼容老版本的tagent的upload不支持路径中的环境变量的情况
-        if (uploadStatus != 0) {
-            //获得tagent安装路径
-            int getTagentHomeStatus = tagentClient.execCmd("echo %TAGENT_BASE% ", null, 10000, handler);
-            if (getTagentHomeStatus != 0) {
-                throw new TagentInstallPathFailedException();
+            //获得升级文件
+            String fileName = paramObj.getString("fileName");
+            MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+            MultipartFile multipartFile = multipartRequest.getFile(fileName);
+            if (multipartFile == null) {
+                throw new TagentPkgNotFoundException(fileName);
             }
-            String path = JSONObject.parseObject(handler.getContent()).getJSONArray("std").getString(0).trim().replaceAll("\n", "");
+            InputStream input = multipartFile.getInputStream();
 
-            //上传tagent安装包
-            uploadStatus = tagentClient.upload(input, fileName, path + "/tmp", null);
+            //获得os类型
+            TagentClient tagentClient = new TagentClient(paramObj.getString("ip"), Integer.parseInt(paramObj.getString("port")), RC4Util.decrypt(paramObj.getString("credential").substring(4)));
+            String osType = tagentClient.getAgentOsType();
+
+            int uploadStatus = tagentClient.upload(input, fileName, "$TAGENT_BASE/tmp", null);
+
+            //兼容老版本的tagent的upload不支持路径中的环境变量的情况
             if (uploadStatus != 0) {
-                throw new TagentUploadPkgFailedException();
+                //获得tagent安装路径
+                int getTagentHomeStatus = tagentClient.execCmd("echo %TAGENT_BASE% ", null, 10000, handler);
+                if (getTagentHomeStatus != 0) {
+                    throw new TagentInstallPathFailedException();
+                }
+                String path = JSONObject.parseObject(handler.getContent()).getJSONArray("std").getString(0).trim().replaceAll("\n", "");
+
+                //上传tagent安装包
+                uploadStatus = tagentClient.upload(input, fileName, path + "/tmp", null);
+                if (uploadStatus != 0) {
+                    throw new TagentUploadPkgFailedException();
+                }
             }
-        }
 
-        // 解压  替换文件 升级
-        int cmdExecStatus = -1;
-        String ignoreFile = paramObj.getString("ignoreFile");
-        if ("windows".equals(osType)) {
-            StringBuilder cmd = new StringBuilder();
-            cmd.append("7z x -ttar -y \"%TAGENT_BASE%\\tmp\\").append(fileName).append("\"");
-            if (StringUtils.isNotBlank(ignoreFile)) {
-                ignoreFile = ignoreFile.trim();
-                cmd.append(" -x!").append(ignoreFile.replaceAll("\\s+", " -x!"));
+            // 解压  替换文件 升级
+            int cmdExecStatus = -1;
+            String ignoreFile = paramObj.getString("ignoreFile");
+            if ("windows".equals(osType)) {
+                StringBuilder cmd = new StringBuilder();
+                cmd.append("7z x -ttar -y \"%TAGENT_BASE%\\tmp\\").append(fileName).append("\"");
+                if (StringUtils.isNotBlank(ignoreFile)) {
+                    ignoreFile = ignoreFile.trim();
+                    cmd.append(" -x!").append(ignoreFile.replaceAll("\\s+", " -x!"));
+                }
+
+                cmd.append(" -o\"%TAGENT_BASE%\" bin lib tools mod");
+                cmdExecStatus = tagentClient.execCmd(cmd.toString(), null, 10000, handler);
+
+            } else {
+                // 切换至agent目录解压升级文件
+                cmdExecStatus = tagentClient.execCmd("cd \"$TAGENT_BASE\" && tar xvf \"$TAGENT_BASE/tmp/" + fileName + "\" bin lib tools mod ", paramObj.getString("user"), 10000, handler);
+            }
+            if (cmdExecStatus != 0) {
+                throw new TagentDecompressionFailedException();
             }
 
-            cmd.append(" -o\"%TAGENT_BASE%\" bin lib tools mod");
-            cmdExecStatus = tagentClient.execCmd(cmd.toString(), null, 10000, handler);
+            //重启
+            int reloadStatus = tagentClient.reload();
 
-        } else {
-            // 切换至agent目录解压升级文件
-            cmdExecStatus = tagentClient.execCmd("cd \"$TAGENT_BASE\" && tar xvf \"$TAGENT_BASE/tmp/" + fileName + "\" bin lib tools mod ", paramObj.getString("user"), 10000, handler);
-        }
-        if (cmdExecStatus != 0) {
-            throw new TagentDecompressionFailedException();
-        }
-
-        //重启
-        int reloadStatus = tagentClient.reload();
-
-        if (reloadStatus != 0) {
-            throw new TagentRestartFailedException();
+            if (reloadStatus != 0) {
+                throw new TagentRestartFailedException();
+            }
+        } catch (Exception e) {
+            throw new TagentUpgradeFailedException(e.getMessage());//待改
         }
         return null;
     }

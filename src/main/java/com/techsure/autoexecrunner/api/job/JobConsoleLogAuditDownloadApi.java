e7164ee754a2ff9b844d8e48d1b430ead1674682
lvzk <lvzk@techsure.com.cn>
2021-12-09 09:46:45 +0800

    add console log audit

---
+++ src/main/java/com/techsure/autoexecrunner/api/job/JobConsoleLogAuditDownloadApi.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright(c) 2021 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+package com.techsure.autoexecrunner.api.job;
+
+import com.alibaba.fastjson.JSONObject;
+import com.techsure.autoexecrunner.common.config.Config;
+import com.techsure.autoexecrunner.constvalue.ApiParamType;
+import com.techsure.autoexecrunner.restful.annotation.Input;
+import com.techsure.autoexecrunner.restful.annotation.Param;
+import com.techsure.autoexecrunner.restful.core.privateapi.PrivateBinaryStreamApiComponentBase;
+import com.techsure.autoexecrunner.util.FileUtil;
+import com.techsure.autoexecrunner.util.JobUtil;
+import com.techsure.autoexecrunner.util.TimeUtil;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.util.Date;
+
+@Component
+public class JobConsoleLogAuditDownloadApi extends PrivateBinaryStreamApiComponentBase {
+
+    @Override
+    public String getToken() {
+        return "/job/console/log/audit/download";
+    }
+
+    @Override
+    public String getName() {
+        return "下载作业节点执行记录";
+    }
+
+    @Input({
+            @Param(name = "jobId", type = ApiParamType.LONG, desc = "作业Id", isRequired = true),
+            @Param(name = "startTime", type = ApiParamType.LONG, desc = "执行开始时间", isRequired = true),
+            @Param(name = "execUser", type = ApiParamType.STRING, desc = "执行用户", isRequired = true),
+    })
+    @Override
+    public Object myDoService(JSONObject jsonObj, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Long jobId = jsonObj.getLong("jobId");
+        Long startTime = jsonObj.getLong("startTime");
+        String execUser = jsonObj.getString("execUser");
+        String logPath = Config.AUTOEXEC_HOME() + File.separator + JobUtil.getJobPath(jobId.toString(), new StringBuilder()) + File.separator + "log" + File.separator + "console.hislog" + File.separator;
+        logPath += TimeUtil.convertDateToString(new Date(startTime), TimeUtil.YYYYMMDD_HHMMSS) + "." + execUser + ".txt";
+        FileUtil.downloadFileByPath(logPath, response);
+        return null;
+    }
+
+}

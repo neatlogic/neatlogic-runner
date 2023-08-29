f090b65ac28efdfe203ee00d562adb18d92107b8
lvzk <897706680@qq.com>
2023-08-29 15:45:44 +0800

    [功能]作业日志接口防止xss攻击 #[965705120317440]作业日志接口防止xss攻击 http://192.168.0.96:8090/demo/rdm.html#/story-detail/939050947543040/939050947543042/965705120317440

---
+++ src/main/java/com/neatlogic/autoexecrunner/util/XssUtil.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.neatlogic.autoexecrunner.util;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class XssUtil {
+
+
+    public static void escapeXss(JSONObject paramObj, String key) {
+        Object value = paramObj.get(key);
+        if (value instanceof String) {
+            try {
+                JSONObject valObj = JSONObject.parseObject(value.toString());
+                escapeXss(valObj);
+                paramObj.replace(key, valObj.toJSONString());
+            } catch (Exception ex) {
+                try {
+                    JSONArray valList = JSONArray.parseArray(value.toString());
+                    encodeHtml(valList);
+                    paramObj.replace(key, valList.toJSONString());
+                } catch (Exception e) {
+                    paramObj.replace(key, escapeXss(value.toString()));
+                }
+            }
+        } else if (value instanceof JSONObject) {
+            escapeXss((JSONObject) value);
+            paramObj.replace(key, value);
+        } else if (value instanceof JSONArray) {
+            encodeHtml((JSONArray) value);
+            paramObj.replace(key, value);
+        }
+    }
+
+    /*
+    private static Pattern scriptPattern = Pattern.compile("<script(.*?)</script>", Pattern.CASE_INSENSITIVE);
+    private static Pattern javascriptPattern = Pattern.compile("javascript:", Pattern.CASE_INSENSITIVE);
+    private static Pattern evalPattern = Pattern.compile("eval\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOLL);
+    */
+
+    public static String escapeXss(String str) {
+        if (StringUtils.isNotBlank(str)) {
+            /*
+             处理js xss注入
+             str = scriptPattern.matcher(str).replaceAll("");
+             str = javascriptPattern.matcher(str).replaceAll("");
+             str = evalPattern.matcher(str).replaceAll("");
+            */
+            //str = str.replace("&", "&amp;");
+            str = str.replace("<", "&lt;");
+            str = str.replace(">", "&gt;");
+            str = str.replace("'", "&#39;");
+            str = str.replace("\"", "&quot;");
+
+            return str;
+        }
+        return "";
+    }
+
+
+    public static void escapeXss(JSONObject j) {
+        Set<String> set = j.keySet();
+        for (String s : set) {
+            String newKey = escapeXss(s);
+            if (!newKey.equals(s)) {
+                Object value = j.get(s);
+                j.remove(s);
+                j.replace(newKey, value);
+            }
+            if (j.get(newKey) instanceof JSONObject) {
+                escapeXss(j.getJSONObject(newKey));
+            } else if (j.get(newKey) instanceof JSONArray) {
+                encodeHtml(j.getJSONArray(newKey));
+            } else if (j.get(newKey) instanceof String) {
+                j.replace(newKey, escapeXss(j.getString(newKey)));
+            }
+        }
+    }
+
+    public static void encodeHtml(JSONArray j) {
+        for (int i = 0; i < j.size(); i++) {
+            if (j.get(i) instanceof JSONObject) {
+                escapeXss(j.getJSONObject(i));
+            } else if (j.get(i) instanceof JSONArray) {
+                encodeHtml(j.getJSONArray(i));
+            } else if (j.get(i) instanceof String) {
+                j.set(i, escapeXss(j.getString(i)));
+            }
+        }
+    }
+
+    // 正则表达式匹配所有以 on 开头的单词，以及引号中的所有内容
+    static String regexRemoveEvent = "\\b(on\\w+\\s*=\\s*)(['\"]?)(.*?)\\2";
+    static Pattern patternRemoveEvent = Pattern.compile(regexRemoveEvent);
+
+    /**
+     * 只去掉事件
+     *
+     * @param html 页面html
+     * @return 去掉事件后的html
+     */
+    public static String removeHtmlEvent(String html) {
+        // 创建Matcher对象
+        Matcher matcher = patternRemoveEvent.matcher(html);
+        // 查找匹配的事件属性
+        while (matcher.find()) {
+            html = html.replace(matcher.group(1) + matcher.group(2) + matcher.group(3)+ matcher.group(2) , StringUtils.EMPTY);
+        }
+        return html;
+    }
+}

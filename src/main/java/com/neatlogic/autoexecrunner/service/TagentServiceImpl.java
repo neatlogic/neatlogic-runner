b7930786b2232b156f8b213c89a13f83d9c9501c
lvzk <897706680@qq.com>
2025-07-29 12:34:47 +0800

    [功能]废弃RestUtil改用HttpRequestUtil

--- src/main/java/com/neatlogic/autoexecrunner/service/TagentServiceImpl.java
+++ src/main/java/com/neatlogic/autoexecrunner/service/TagentServiceImpl.java
@@ -45,7 +45,7 @@ public class TagentServiceImpl implements TagentService {
                 status = true;
                 jsonObj.put("data", resultJson);
             } else {
-                execInfo.append("Server Error,").append(httpResult);
+                execInfo.append("Server Error,").append(httpRequestUtil.getResult());
             }
         } else {
             execInfo.append("neatlogic return message is blank");
